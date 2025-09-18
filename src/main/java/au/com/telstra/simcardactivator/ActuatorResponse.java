2355681defbed0dd3cc53ce5f2a2084ac1eab69a
Ismail Iqbal <ismail.h.iqbal@gmail.com>
2025-09-18 15:22:55 -0500

    Add SIM activation REST controller and DTOs

---
+++ src/main/java/au/com/telstra/simcardactivator/ActuatorResponse.java
@@ -0,0 +1,8 @@
+package com.telstra.simcardactivator;
+
+public class ActuatorResponse {
+    private boolean success;
+
+    public boolean isSuccess() { return success; }
+    public void setSuccess(boolean success) { this.success = success; }
+}
